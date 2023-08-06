a578803a7ccdf21ed1d433f79f1777155f8889b8
Ravi <ravi.kmr.singh8@hotmail.com>
2023-08-06 18:35:53 +0530

    Enable Actuator endpoints

--- spring-boot-basics/src/main/java/com/example/errors/CustomResponseEntityExceptionHandler.java
+++ spring-boot-basics/src/main/java/com/example/errors/CustomResponseEntityExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -13,11 +14,22 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = UserNotFound.class)
-    public ResponseEntity<ProblemDetail> handleUserNotFoundException(UserNotFound ex) {
+    // Handle all unknown Errors as Internal Server Error
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetail> handleAllException(Exception ex, WebRequest request) {
+        ProblemDetail details = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        details.setProperty("path", ServletUriComponentsBuilder.fromCurrentRequest().build().toUri());
+        details.setProperty("timestamp", LocalDateTime.now());
+        return ResponseEntity.of(details).build();
+    }
+
+    // Handle UserNotFound
+    @ExceptionHandler(UserNotFound.class)
+    public ResponseEntity<ProblemDetail> handleUserNotFoundException(UserNotFound ex, WebRequest request) {
         ProblemDetail details = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         details.setProperty("path", ServletUriComponentsBuilder.fromCurrentRequest().build().toUri());
         details.setProperty("timestamp", LocalDateTime.now());
         return ResponseEntity.of(details).build();
     }
 }
+
